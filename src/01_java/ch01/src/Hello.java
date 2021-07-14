da5a4540bf6131b52e28e5d1498fc373f60b3c4b
yoonjihwan1328 <83949801+yoonjihwan1328@users.noreply.github.com>
2021-07-14 17:39:20 +0900

    Add files via upload

---
+++ src/01_java/ch01/src/Hello.java
@@ -0,0 +1,10 @@
+// (주석 한줄짜리 주석)
+/* 여러줄짜리 주석
+ *  java파일명과 클래스명은 같아야함
+ */
+public class Hello {
+	//JVM main() 함수를 실행
+	public static void main(String[] args) {
+		System.out.println("Hello, World!") ; 
+	}
+}
